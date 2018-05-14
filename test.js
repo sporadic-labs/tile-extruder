f7b927ffe4deef684d7c416c6b6388b73cc1ee7f
Michael Hadley <michaelwesthadley@gmail.com>
2018-05-14 08:39:28 -0500

    Add 8x16 test to catch width != height bugs

--- test.js
+++ test.js
@@ -12,7 +12,8 @@ const tilesets = [
   { path: "./tilesets/buch-tileset", w: 16, h: 16 },
   { path: "./tilesets/arachne-tileset", w: 8, h: 8 },
   { path: "./tilesets/mario-tileset", w: 16, h: 16 },
-  { path: "./tilesets/test-tileset", w: 64, h: 64 }
+  { path: "./tilesets/test-tileset", w: 64, h: 64 },
+  { path: "./tilesets/borders-tileset", w: 8, h: 16 }
 ];
 
 tilesets.map(({ path, w, h }) => {
