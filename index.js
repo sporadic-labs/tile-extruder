7d980aab2714a6ba49c6d7b394f78731721fd20f
Michael Hadley <michaelwesthadley@gmail.com>
2018-05-14 19:49:11 -0500

    Add ability to use margin & spacing

--- index.js
+++ index.js
@@ -24,11 +24,31 @@ program
     "RGBA hex color to use for the background color (defaults to transparent)",
     toHex
   )
+  .option(
+    "-m, --margin [integer=0]",
+    "number of pixels between tiles and the edge of the tileset image (defaults to 0)",
+    toInt,
+    0
+  )
+  .option(
+    "-s, --spacing [integer=0]",
+    "number of pixels between neighboring tiles (defaults to 0)",
+    toInt,
+    0
+  )
   .option("-i, --input <path>", "the path to the tileset you want to extrude")
   .option("-o, --output <path>", "the path to output the extruded tileset image")
   .parse(process.argv);
 
-const { tileWidth, tileHeight, input: inputPath, output: outputPath } = program;
+const {
+  tileWidth,
+  tileHeight,
+  margin,
+  spacing,
+  color,
+  input: inputPath,
+  output: outputPath
+} = program;
 
 if (!tileWidth) {
   console.log("\nMissing tileWidth! See help below for usage information:");
@@ -54,19 +74,32 @@ if (!outputPath) {
   });
 
   const { width, height } = image.bitmap;
-  const cols = width / tileWidth;
-  const rows = height / tileHeight;
-  const newWidth = (tileWidth + 2) * cols;
-  const newHeight = (tileHeight + 2) * rows;
+
+  // Solve for "cols" & "rows" to get the formula used here
+  //  width = 2 * margin + (cols - 1) * spacing + cols * tileWidth
+  //  height = 2 * margin + (rows - 1) * spacing + rows * tileHeight
+  const cols = (width - 2 * margin + spacing) / (tileWidth + spacing);
+  const rows = (height - 2 * margin + spacing) / (tileHeight + spacing);
+
+  if (!Number.isInteger(cols) || !Number.isInteger(rows)) {
+    console.error(
+      "Non-integer number of rows or cols found. The image doesn't match the specified parameters. Double check your margin, spacing, tileWidth and tileHeight."
+    );
+    process.exit(1);
+  }
+
+  // Same calculation but in reverse & inflating the tile size by the extrusion amount
+  const newWidth = 2 * margin + (cols - 1) * spacing + cols * (tileWidth + 2);
+  const newHeight = 2 * margin + (rows - 1) * spacing + rows * (tileHeight + 2);
 
   const extrudedImage = await new Jimp(newWidth, newHeight, color);
 
   for (let row = 0; row < rows; row++) {
     for (let col = 0; col < cols; col++) {
-      let srcX = col * tileWidth;
-      let srcY = row * tileHeight;
-      let destX = col * (tileWidth + 2);
-      let destY = row * (tileHeight + 2);
+      let srcX = margin + col * (tileWidth + spacing); // x of tile top left
+      let srcY = margin + row * (tileHeight + spacing); // y of tile top left
+      let destX = margin + col * (tileWidth + spacing + 2); // x of the extruded tile top left
+      let destY = margin + row * (tileHeight + spacing + 2); // y of the extruded tile top left
       const tw = tileWidth;
       const th = tileHeight;
 
