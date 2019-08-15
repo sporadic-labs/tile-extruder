c0ff4b028fb32af5bf7a6aeaecf83a865f1e4a0b
Michael Hadley <michaelwesthadley@gmail.com>
2019-08-15 09:25:37 -0500

    Mv extruded tilesets to their own subdirectory

--- test.js
+++ test.js
@@ -9,19 +9,19 @@ const cb = (err, stdout, stderr) => {
 };
 
 const tilesets = [
-  { i: "./tilesets/buch-tileset.png", o: "./tilesets/buch-tileset-extruded.png", w: 16, h: 16 },
-  { i: "./tilesets/arachne-tileset.png", o: "./tilesets/arachne-tileset-extruded.png", w: 8, h: 8 },
-  { i: "./tilesets/mario-tileset.png", o: "./tilesets/mario-tileset-extruded.png", w: 16, h: 16 },
-  { i: "./tilesets/test-tileset.png", o: "./tilesets/test-tileset-extruded.png", w: 64, h: 64 },
+  { i: "./tilesets/buch-tileset.png", o: "./tilesets/extruded/buch-tileset.png", w: 16, h: 16 },
+  { i: "./tilesets/arachne-tileset.png", o: "./tilesets/extruded/arachne-tileset.png", w: 8, h: 8 },
+  { i: "./tilesets/mario-tileset.png", o: "./tilesets/extruded/mario-tileset.png", w: 16, h: 16 },
+  { i: "./tilesets/test-tileset.png", o: "./tilesets/extruded/test-tileset.png", w: 64, h: 64 },
   {
     i: "./tilesets/borders-tileset.png",
-    o: "./tilesets/borders-tileset-extruded.png",
+    o: "./tilesets/extruded/borders-tileset.png",
     w: 8,
     h: 16
   },
   {
     i: "./tilesets/mario-tileset-10-spacing-5-margin.png",
-    o: "./tilesets/mario-tileset-10-spacing-5-margin-extruded.png",
+    o: "./tilesets/extruded/mario-tileset-10-spacing-5-margin.png",
     w: 16,
     h: 16,
     m: 5,
