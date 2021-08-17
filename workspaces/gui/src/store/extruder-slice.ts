19461b2c807dc11613009d46117979991e86cbb6
Michael Hadley <michaelwesthadley@gmail.com>
2021-08-17 12:04:54 -0500

    Update store to keep track of input image info (including ref to ImageStorage)

--- workspaces/gui/src/store/extruder-slice.ts
+++ workspaces/gui/src/store/extruder-slice.ts
@@ -1,27 +1,52 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { ImageId } from "../image-storage/image-storage";
 
 interface ExtruderState {
   width: number;
   height: number;
+  name: string;
+  type: string;
+  imageStorageId: ImageId | null;
 }
 
 const initialState: ExtruderState = {
   width: 0,
   height: 0,
+  name: "",
+  type: "",
+  imageStorageId: null,
 };
+
+interface InputImagePayload {
+  width: number;
+  height: number;
+  name: string;
+  type: string;
+  imageId: ImageId;
+}
 
 const extruderSlice = createSlice({
   name: "extruder",
   initialState,
   reducers: {
-    setSize: (state, action: PayloadAction<{ width: number; height: number }>) => {
+    clearInputImage: (state) => {
+      state.width = 0;
+      state.height = 0;
+      state.name = "";
+      state.type = "";
+      state.imageStorageId = null;
+    },
+    setInputImage: (state, action: PayloadAction<InputImagePayload>) => {
       state.width = action.payload.width;
       state.height = action.payload.height;
+      state.name = action.payload.name;
+      state.type = action.payload.type;
+      state.imageStorageId = action.payload.imageId;
     },
   },
 });
 
 export default extruderSlice.reducer;
-export const { setSize } = extruderSlice.actions;
+export const { setInputImage, clearInputImage } = extruderSlice.actions;
 export { extruderSlice };
-export type { ExtruderState };
+export type { ExtruderState, InputImagePayload };
