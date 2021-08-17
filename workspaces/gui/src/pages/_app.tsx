4346b82e0b0a46f9f604690cfad958ef4dc227d1
Michael Hadley <michaelwesthadley@gmail.com>
2021-08-17 15:23:27 -0500

    Add some global styling

--- workspaces/gui/src/pages/_app.tsx
+++ workspaces/gui/src/pages/_app.tsx
@@ -3,6 +3,10 @@ import { store } from "../store";
 import SiteLayout from "../components/site-layout";
 import type { AppProps } from "next/app";
 import { ImageStorageProvider } from "../image-storage/react-integration";
+import "@fontsource/raleway/400.css";
+import "@fontsource/raleway/400-italic.css";
+import "@fontsource/raleway/700.css";
+import "@fontsource/raleway/700-italic.css";
 import "../global-styles/index.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
