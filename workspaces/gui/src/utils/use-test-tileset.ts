a51139c2f9a9ef2330b96324b494ca5a7d6752fe
Michael Hadley <michaelwesthadley@gmail.com>
2021-08-20 06:48:00 -0500

    Update useTestTileset to dispatch latest action

--- workspaces/gui/src/utils/use-test-tileset.ts
+++ workspaces/gui/src/utils/use-test-tileset.ts
@@ -1,28 +1,19 @@
-import { setInputImage, setTileWidth, setTileHeight } from "../store/extruder-slice";
-import { useAppSelector, useAppDispatch } from "../store/hooks";
-import { useImageStorage } from "../store/image-storage/react-integration";
-import testTileset from "../assets/tilesets/test-tileset.png";
+import { setTileWidth, setTileHeight, setImageFromFile } from "../store/extruder-slice";
+import { useAppDispatch } from "../store/hooks";
 import { useEffect } from "react";
 
 export default function useTestTileset(imagePath: string, tileWidth: number, tileHeight: number) {
   const dispatch = useAppDispatch();
-  const imageStorage = useImageStorage();
 
   useEffect(() => {
     async function addTest() {
-      const [id, data] = await imageStorage.addFromPath(imagePath);
-      dispatch(
-        setInputImage({
-          width: data.width,
-          height: data.height,
-          name: "Test Tileset",
-          type: "image/png",
-          imageId: id,
-        })
-      );
+      const res = await fetch(imagePath);
+      const blob = await res.blob();
+      const file = new File([blob], imagePath);
+      dispatch(setImageFromFile(file));
       dispatch(setTileWidth(tileWidth));
       dispatch(setTileHeight(tileHeight));
     }
     addTest();
-  }, []);
+  }, [dispatch, imagePath, tileWidth, tileHeight]);
 }
