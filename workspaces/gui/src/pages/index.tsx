ca4771d569c176a061c84f1ea9bb36500ab8b8e5
Michael Hadley <michaelwesthadley@gmail.com>
2021-08-20 06:48:15 -0500

    Update home to dispatch latest action

--- workspaces/gui/src/pages/index.tsx
+++ workspaces/gui/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import DropZone from "../features/file-drop-zone";
 import { useImageStorage } from "../store/image-storage/react-integration";
 import ExtrudeForm from "../features/extrude-form";
-import { setInputImage, setTileHeight, setTileWidth } from "../store/extruder-slice";
+import { setImageFromFile } from "../store/extruder-slice";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
 import testTileset from "../assets/tilesets/test-tileset.png";
 import useTestTileset from "../utils/use-test-tileset";
@@ -10,22 +10,14 @@ export default function Home() {
   const imageStorageId = useAppSelector((state) => state.extruder.imageStorageId);
   const dispatch = useAppDispatch();
   const imageStorage = useImageStorage();
+
   useTestTileset(testTileset.src, 64, 64);
 
   const hasUploadedImage = imageStorageId && imageStorage.has(imageStorageId);
 
   const onFile = async (file: File) => {
     try {
-      const [id, data] = await imageStorage.addFromFile(file);
-      dispatch(
-        setInputImage({
-          width: data.width,
-          height: data.height,
-          name: file.name,
-          type: file.type,
-          imageId: id,
-        })
-      );
+      dispatch(setImageFromFile(file));
     } catch (err) {
       console.log(err);
       // TODO: error needs to be passed back to drop zone, or refactored.
