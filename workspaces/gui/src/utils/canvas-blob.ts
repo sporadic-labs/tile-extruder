8e75e58d22dadc3c32efbc7fbd655bb42240a393
Michael Hadley <michaelwesthadley@gmail.com>
2021-08-21 15:56:51 -0500

    Update canvas blob utils to rely on better types

--- workspaces/gui/src/utils/canvas-blob.ts
+++ workspaces/gui/src/utils/canvas-blob.ts
@@ -1,4 +1,4 @@
-const possibleCanvasBlobTypes = ["image/png", "image/jpeg", "image/webp", "image/bmp", "image/gif"];
+import { ImageMimeType, imageMimeTypes } from "./image-filename-utils";
 
 async function canvasToBlob(canvas: HTMLCanvasElement, type?: string, quality?: any) {
   return new Promise<Blob>((resolve, reject) => {
@@ -14,7 +14,7 @@ async function canvasToBlob(canvas: HTMLCanvasElement, type?: string, quality?: 
 }
 
 async function getSupportedCanvasBlobTypes() {
-  const supportedFormats: string[] = [];
+  const supportedFormats: ImageMimeType[] = [];
 
   const canvas = document.createElement("canvas");
   canvas.width = 1;
@@ -29,7 +29,7 @@ async function getSupportedCanvasBlobTypes() {
   ctx.fillStyle = "blue";
   ctx.fillRect(0, 0, 1, 1);
 
-  for (const type of possibleCanvasBlobTypes) {
+  for (const type of imageMimeTypes) {
     try {
       const blob = await canvasToBlob(canvas, type);
       if (blob.type === type) {
