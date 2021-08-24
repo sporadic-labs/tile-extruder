4aeff2fece36313bd1ac4eac17c89c0a37ca94d1
Michael Hadley <michaelwesthadley@gmail.com>
2021-08-24 12:52:48 -0500

    Create typed hook for getting access to root store

--- workspaces/gui/src/store/hooks.ts
+++ workspaces/gui/src/store/hooks.ts
@@ -1,5 +1,6 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import type { RootState, AppDispatch } from "./index";
+import { TypedUseSelectorHook, useDispatch, useSelector, useStore } from "react-redux";
+import { RootState, AppDispatch } from "./index";
 
+export const useAppStore = () => useStore<RootState>();
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
