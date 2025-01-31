ced86afe2dd3259016d05f8f96a81b84af087087
Michael Hadley <michaelwesthadley@gmail.com>
2025-01-31 13:44:17 -0600

    Next app generation (#46)

---
+++ packages/tile-extruder-app/README.md
@@ -0,0 +1,9 @@
+# Tile Extruder Web App
+
+A web app for extruding tilesets.
+
+## Running
+
+Install pnpm and then run the following commands from the root of the repo:
+- `pnpm install`
+- `pnpm --filter tile-extruder-app dev`
---
+++ packages/tile-extruder-app/src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-montserrat",
+});
+
+export const metadata: Metadata = {
+  title: "Tile Extruder",
+  description: `An app for extruding tiles in tilesets to avoid "bleeding" issues where tiles are rendered with the wrong color at the edges of tiles.`,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.variable}`}>{children}</body>
+    </html>
+  );
+}
