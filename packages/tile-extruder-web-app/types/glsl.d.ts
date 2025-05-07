35e526e73822618429c0e8f32c87338dd115f75f
Michael Hadley <michaelwesthadley@gmail.com>
2025-05-07 16:45:03 -0500

    Fix

--- packages/tile-extruder-web-app/types/glsl.d.ts
+++ packages/tile-extruder-web-app/types/glsl.d.ts
@@ -2,13 +2,3 @@ declare module "*.glsl" {
   const content: string;
   export default content;
 }
-
-declare module "*.vert" {
-  const content: string;
-  export default content;
-}
-
-declare module "*.frag" {
-  const content: string;
-  export default content;
-}
